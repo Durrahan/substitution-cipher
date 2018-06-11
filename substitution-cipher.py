77c4fdf8ffc2ad0a17c1106dcee2a02a2f12940e
Kevin Koch <Durrahan@Kevins-iMac.local>
2018-06-12 01:25:23 +0200

    Formated code.

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -8,31 +8,31 @@ import argparse
 
 def to_array(text):
     """Convert string to ascii array.
-    
+
     Args:
         text: A string containing only uppercase and lowercase letters.
     Returns:
         A list of integers mapped to the corresponding ascii value. 
-    
+
     """
     return [ord(x) - 96 for x in text.lower() if x >= "a" and x <= "z"]
 
 
 def to_text(array):
     """Convert ascii array to string.
-    
+
     Args:
         array: A list of integers mapped to the corresponding ascii value. 
     Returns:
         A string containing the corresponding lowercase letters from the ascii table. 
-    
+
     """
     return "".join([chr(int(x) + 96) for x in array])
 
 
 def encrypt(key, text):
     """Encrypt plaintext using a keyphrase.
-    
+
     A substitution cipher with a keyphrase is used to encrypt a list of integers. 
     The keyphrase will be repeated if the text exceeds it's length.
 
@@ -42,14 +42,14 @@ def encrypt(key, text):
         text: A list of integers mapped to the corresponding ascii value. 
     Returns:
         A list of integers representing the encrypted text.
-    
+
     """
     return [(text[x] + key[x % len(key)]) % 26 for x in range(len(text))]
 
 
 def decrypt(key, cipher):
     """Decrypt cipher using a keyphrase.
-    
+
     A substitution cipher with a keyphrase is used to decrypt a list of integers. 
     The keyphrase will be repeated if the text exceeds it's length.
 
@@ -59,7 +59,7 @@ def decrypt(key, cipher):
         cipher: A list of integers representing the encrypted text.
     Returns:
         A list of integers mapped to the corresponding ascii value. 
-    
+
     """
     return [(int(cipher[x]) - key[x % len(key)]) % 26 for x in range(len(cipher))]
 
